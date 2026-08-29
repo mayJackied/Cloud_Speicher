5ab805b7079447bf2676965e008bed3e89bed568
Jackied <2382408129@qq.com>
2026-08-29 14:32:43 +0800

    用户名 / 密码约束 OK 布尔字段命名 OK

--- src/main/java/com/zuantou/pojo/InviteCode.java
+++ src/main/java/com/zuantou/pojo/InviteCode.java
@@ -1,6 +1,8 @@
 package com.zuantou.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,5 +15,7 @@ import lombok.NoArgsConstructor;
 public class InviteCode {
     @TableId
     private String inviteCode;
-    private boolean isDelete;
+    @TableLogic
+    @TableField("is_deleted")
+    private boolean deleted;
 }
--- src/main/java/com/zuantou/pojo/User.java
+++ src/main/java/com/zuantou/pojo/User.java
@@ -1,8 +1,6 @@
 package com.zuantou.pojo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +14,10 @@ public class User {
     private Integer userId;
     private String password;
     private String name;
-    private boolean isDelete;
+    @TableLogic
+    @TableField("is_deleted")
+    private boolean deleted;
     private Integer documentId;
-    private boolean isAdmin;
+    @TableField("is_admin")
+    private boolean admin;
 }
